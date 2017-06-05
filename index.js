3874b5ce2d20e9af76b35bb973235d2ab3fa0ce9
Daniel Holmlund <agnathan@gmail.com>
2017-06-05 16:12:24 -0700

    Remove CloudProviderSchema and SensorCloudSchema

--- index.js
+++ index.js
@@ -28,14 +28,12 @@ var SensorSchema = require('./schema/sensorSchema.js');
 var ActuatorSchema = require('./schema/actuatorSchema.js');
 var TriggerSchema = require('./schema/triggerSchema.js');
 var DataSchema = require('./schema/dataSchema.js');
-var SensorCloudSchema = require('./schema/sensorCloudSchema.js');
 var ErrorSchema = require('./schema/errorSchema.js');
 
 module.exports = {
     SensorModel: mongoose.model('SensorModel', SensorSchema),
     ActuatorModel: mongoose.model('ActuatorModel', ActuatorSchema),
     DataModel: mongoose.model('DataModel', DataSchema),
-    SensorCloudModel: mongoose.model('SensorCloudModel', SensorCloudSchema),
     TriggerModel: mongoose.model('TriggerModel', TriggerSchema),
     ErrorModel:  mongoose.model('ErrorModel', ErrorSchema)
 };
